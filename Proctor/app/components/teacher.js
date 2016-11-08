6a84cf0037627af33e9e1f8c920bdf4d0dd85ba1
Tyler Collette <tyler.collette@gmail.com>
2016-11-07 22:45:33 -0700

    Working on Teacher layout - menus

--- Proctor/app/components/teacher.js
+++ Proctor/app/components/teacher.js
@@ -5,10 +5,35 @@
 angular.module('teacher', [])
     .component('teacher', {
         templateUrl: 'app/views/teacher/index.html',
+        controller: TeacherController,
         $routeConfig: [
-            {path: '/', name: 'TeacherDashboard', component: 'teacherDashboard', useAsDefault: true}
+            {path: '/', name: 'TeacherDashboard', component: 'teacherDashboard', useAsDefault: true},
+            {path: '/review', name: 'TeacherReview', component: 'teacherReview'},
+            {path: '/syllabus', name: 'TeacherSyllabus', component: 'teacherSyllabus'}
         ]
     })
     .component('teacherDashboard', {
-        templateUrl: 'app/views/teacher/dashboard.html'
+        templateUrl: 'app/views/teacher/dashboard.html',
+        controller: TeacherDashboardController
     })
+    .component('teacherReview', {
+        templateUrl: 'app/views/teacher/review.html',
+        controller: TeacherReviewController
+    })
+    .component('teacherSyllabus',{
+        templateUrl: 'app/views/teacher/syllabus.html',
+        controller: TeacherSyllabusController
+    });
+
+function TeacherController() {
+}
+
+function TeacherDashboardController() {
+}
+
+function TeacherReviewController() {
+}
+
+function TeacherSyllabusController() {
+}
+
