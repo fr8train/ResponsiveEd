0d85b9456a3615938dc15588d89004ccd6f2a773
Tyler Collette <tyler.collette@gmail.com>
2016-12-01 22:56:02 -0700

    Working on default routing on 'root'

--- Proctor/routing.js
+++ Proctor/routing.js
@@ -5,22 +5,32 @@ angular.module('proctor')
     .config(['$stateProvider', '$urlRouterProvider',
         function ($stateProvider, $urlRouterProvider) {
             $stateProvider
+                .state('root', {
+                    url: '/'
+                })
                 .state('teacher', {
                     component: 'teacher.component'
                 })
-                .state('teacher.dashboard',{
+                .state('teacher.dashboard', {
                     url: '/teacher',
                     component: 'teacher.dashboard.component'
                 })
-                .state('teacher.review',{
-                    url:'/teacher/review',
+                .state('teacher.review', {
+                    url: '/teacher/review',
                     component: 'teacher.review.component'
                 })
-                .state('teacher.syllabus',{
+                .state('teacher.syllabus', {
                     url: '/teacher/syllabus',
                     component: 'teacher.syllabus.component'
                 });
 
-            $urlRouterProvider.otherwise('/teacher');
+            $urlRouterProvider.otherwise('/');
         }
-    ]);
+    ])
+    .run(['$state', '$transitions', '$rootScope', function ($state, $transitions, $rootScope) {
+        $transitions.onBefore({to: 'root'}, function (transition) {
+            console.log(window.location.href);
+            console.log(transition.$to());
+            return transition.router.stateService.target('teacher.dashboard');
+        })
+    }]);
